c5e43050dbf99e72803ca999106c438a4c1270b3
gregory <gregory@chateaupageot.com>
2019-05-05 17:26:14 +0900

    Add support for label Clean up unused code path Add comment Add logging for P4 commands

--- Pipelines/Compile.java
+++ Pipelines/Compile.java
@@ -3,90 +3,21 @@
 // Gregory Pageot
 // 2018-07-23
 
-// Using p4publish allow to avoid the 5minute command timeout issue
-// Demerit being that it requires to have 1 available workspace license (as a temporary one will be created)
-
-// Will create a new changelist. Return its CL number
-// WARNING: this will include ALL files in the newly created CL, including files in pending changelists(TODO : only include files in Default changelist)
-def CreateCLFromDefault(p4, String p4WorkspaceName, String description)
+def GetListOfClientFile(P4CommandResult)
 {
-	def job = p4.run('change', '-o')
-
-	for(def item : job)
+	String result = ""
+	for(def item : P4CommandResult)
 	{
-		// Change description field
-		def desc = item.get('Description')
-		//echo desc
-		desc = description
-		//echo desc
-		item.put('Description', desc)
-
-		p4.save('change', item)
-	}
-
-	// List pending changelist on current P4 workspace
-	def changelistnumber = -1
-	def jobAllCL = p4.run('changes', '-s', 'pending', '-c', p4WorkspaceName)
-	for(def item : jobAllCL)
-	{
-		changelistnumber = item.get("change")
-		break // First line is the last create changelist
-	}
-	return changelistnumber
-}
-
-def CreateCLFromDefaultFilterered(p4, String p4WorkspaceName, String description, String[] paths)
-{
-	def clNumber = CreateCLFromDefault(p4,  p4WorkspaceName, description)
-
-	// move all files back to default CL
-	p4.run('reopen', '-c', 'default', '//...')
-
-	paths.each {
-		p4.run('reopen', '-c', clNumber, it)
-	}
-
-	return clNumber
-}
-
-@NonCPS
-def readDir(String folder)
-{
-	def dirslist = []
-	new File(folder).eachDir()
-	{ dir ->
-		if (!dir.getName().startsWith('.')) {
-			dirslist.add(dir.getName())
-		}
-	}
-	return dirslist
-}
-
-def p4AddFilesInFolder(p4, String folder)
-{
-	def addedFiles = p4.run('add', "${folder}\\...".toString())
-
-	String addFilesReport = ""
-	for(def item : addedFiles) {
-		for (String key : item.keySet()) {
-			if(key == 'clientFile') {
+		for (String key : item.keySet())
+		{
+			if(key == 'clientFile')
+			{
 				value = item.get(key)
-				addFilesReport += value + "\n"
+				result += value + "\n"
 			}
-			// //value = item.get(key)
-			// //println ("Key: " + key + " Value: " + value)
 		}
 	}
-	echo addFilesReport
-}
-
-def p4AddBinariesForPlugins(p4, String PluginsFolder)
-{
-	String[] pluginList = readDir(PluginsFolder)
-	for(def pluginName : pluginList) {
-		echo "Adding binaries for plugin: ${pluginName}"
-		p4AddFilesInFolder(p4, "${PluginsFolder}\\${pluginName}\\Binaries".toString())
-	}
+	return result
 }
 
 def GetChangelistsDesc()
@@ -126,7 +57,7 @@ def GetPreviousBuildStatusExceptAborted()
 
 node
 {
-	// TODO : comment
+	// If true, only the engine will be built
 	def engineOnly = ENGINE_ONLY.toBoolean()
 	// Path to Jenkins local engine folder for the given perforce workspace
 	def engineLocalPath = ENGINE_LOCAL_PATH
@@ -142,44 +73,32 @@ node
 	def perforceCredentialInJenkins = JENKINS_P4_CREDENTIAL
 	// Encoding for the given perforce workspace
 	def perforceUnicodeMode = P4_UNICODE_ENCODING
-	// TODO : comment
+	// If true, the perforce workspace will do a force clean
 	def optionFullRebuild = FULL_REBUILD.toBoolean()
-	try{
 
+	def perforceLabelName = P4_LABEL_NAME
+	def perforceLabelDescription = P4_LABEL_DESC
+
+	try
+	{
 		stage('Get perforce')
 		{
+			def populateOption
 			if(optionFullRebuild)
 			{
 				// Currently failing with "ERROR: P4: Task Exception: java.io.IOException: Unable to delete directory [Perforce_Workspace_Folder]."
-				checkout perforce(
-					credential: perforceCredentialInJenkins,
-					populate: forceClean(have: true, parallel: [enable: false, minbytes: '1024', minfiles: '1', threads: '4'], pin: '', quiet: true, revert: true),
-					workspace: staticSpec(charset: perforceUnicodeMode, name: perforceWorkspaceName, pinHost: false)
-					)
+				populateOption = forceClean(have: true, parallel: [enable: false, minbytes: '1024', minfiles: '1', threads: '4'], pin: '', quiet: true, revert: true)
 			}
 			else
 			{
-				checkout perforce(
-					credential: perforceCredentialInJenkins,
-					populate: syncOnly(force: false, have: true, modtime: true, parallel: [enable: false, minbytes: '1024', minfiles: '1', threads: '4'], pin: '', quiet: true, revert: true),
-					workspace: staticSpec(charset: perforceUnicodeMode, name: perforceWorkspaceName, pinHost: false)
-					)
+				populateOption = syncOnly(force: false, have: true, modtime: true, parallel: [enable: false, minbytes: '1024', minfiles: '1', threads: '4'], pin: '', quiet: true, revert: true)
 			}
 
-			String checkoutReport = ""
-			echo "currentBuild.changeSets size ${currentBuild.changeSets.size()}"
-			for(def item : currentBuild.changeSets) {
-				checkoutReport += item
-			}
-			echo checkoutReport
-
-			String checkoutReportAuthors = ""
-			Set authors = currentBuild.changeSets.collect({ it.items.collect { it.author.toString() } })
-			Set uniqueAuthors = authors.unique()
-			for(def item : uniqueAuthors) {
-				checkoutReportAuthors += item
-			}
-			echo checkoutReportAuthors
+			checkout perforce(
+				credential: perforceCredentialInJenkins,
+				populate: populateOption,
+				workspace: staticSpec(charset: perforceUnicodeMode, name: perforceWorkspaceName, pinHost: false)
+				)
 		}
 
 		def p4 = p4 credential: perforceCredentialInJenkins, workspace: staticSpec(charset: perforceUnicodeMode, name: perforceWorkspaceName, pinHost: false)
@@ -187,20 +106,12 @@ node
 		stage( 'Prepare' )
 		{
 			// Edit files necessary for GenerateProjectFiles batch
+			echo "edit UnrealBuildTool.xml and UnrealBuildTool.exe.config"
 			def editedFiles = p4.run('edit', 
 				"${engineLocalPath}/Engine/Binaries/DotNET/UnrealBuildTool.xml".toString(),
 				"${engineLocalPath}/Engine/Binaries/DotNET/UnrealBuildTool.exe.config".toString()
 				)
-			String editFilesReport = ""
-			for(def item : editedFiles) {
-				for (String key : item.keySet()) {
-					if(key == 'clientFile') {
-						value = item.get(key)
-						editFilesReport += value + "\n"
-					}
-				}
-			}
-			echo editFilesReport
+			echo GetListOfClientFile(editedFiles)
 
 			// We need to use double quote for batch path in case engine path contains space
 			// /D				Change drive at same time as current folder		
@@ -225,6 +136,7 @@ node
 		// Unnecessary if we use p4publish and if '+w' P4 filetype flag is setup properly for all those files
 		stage('Checkout')
 		{
+			echo "edit engine binaries"
 			def editedFiles = p4.run('edit', 
 				"${engineLocalPath}/Engine/Binaries/Win64/....exe".toString(),
 				"${engineLocalPath}/Engine/Binaries/Win64/....dll".toString(),
@@ -235,19 +147,12 @@ node
 				"${engineLocalPath}/Engine/Plugins/.../Binaries/Win64/....dll".toString(),
 				"${engineLocalPath}/Engine/Plugins/.../Binaries/Win64/....modules".toString()
 				)
-			String editFilesReport = ""
-			for(def item : editedFiles) {
-				for (String key : item.keySet()) {
-					if(key == 'clientFile') {
-						value = item.get(key)
-						editFilesReport += value + "\n"
-					}
-				}
-			}
-			echo editFilesReport
+
+			echo GetListOfClientFile(editedFiles)
 
 			if(engineOnly == false)
 			{
+				echo "edit project binaries"
 				editedFiles = p4.run('edit', 
 					"${projectLocalPath}/${projectName}.uproject".toString(),
 					"${projectLocalPath}/Binaries/Win64/....exe".toString(),
@@ -260,16 +165,8 @@ node
 					"${projectLocalPath}/Plugins/.../Binaries/Win64/....pdb".toString(),
 					"${projectLocalPath}/Plugins/.../Binaries/Win64/....modules".toString()
 					)
-				editFilesReport = ""
-				for(def item : editedFiles) {
-					for (String key : item.keySet()) {
-						if(key == 'clientFile') {
-							value = item.get(key)
-							editFilesReport += value + "\n"
-						}
-					}
-				}
-				echo editFilesReport
+
+				echo GetListOfClientFile(editedFiles)
 			}
 		}
 
@@ -292,127 +189,27 @@ node
 					"""
 			}
 		}
-		/*
-		// unnecessary since if we use p4publish
-		stage('Add new files')
-		{
-			// Add new binaries for new plugins, or after engine merge
-			// WARNING: dll/pdb will not be removed for plugins or engine modules remove!
-			p4AddFilesInFolder(p4, "${engineLocalPath}\\Engine\\Binaries\\Win64")
-
-			p4AddBinariesForPlugins(p4, "${engineLocalPath}\\Engine\\Plugins")
-
-			if(engineOnly == false)
-			{
-				p4AddFilesInFolder(p4, "${projectLocalPath}\\Binaries\\Win64")
-
-				// EXP files should not be added to source control
-				echo "Revert EXP file"
-				def revertedFiles = p4.run('revert', "${projectLocalPath}\\Binaries\\Win64\\*.exp".toString())
-				String revertFilesReport = ""
-				for(def item : revertedFiles) {
-					for (String key : item.keySet()) {
-						if(key == 'clientFile') {
-							value = item.get(key)
-							revertFilesReport += value + "\n"
-						}
-					}
-				}
-				echo revertFilesReport
-
-				p4AddBinariesForPlugins(p4, "${projectLocalPath}\\Plugins")
-			}
-		}*/
 
 		stage('Submit')
 		{
 			// Revert while keeping files in order that the 2nd workspace submit those files
-			echo "Revert -k"
+			echo "Revert -k //..."
 			def revertedFiles = p4.run('revert', '-k', '//...')
-			String revertFilesReport = ""
-			for(def item : revertedFiles) {
-				for (String key : item.keySet()) {
-					if(key == 'clientFile') {
-						value = item.get(key)
-						revertFilesReport += value + "\n"
-					}
-				}
-			}
-			echo revertFilesReport
+			echo GetListOfClientFile(revertedFiles)
 
 			// Problem p4 publish do a "revert -k", "sync -k", "reconcile -e -a -f"
-			// which  will result in adding saved/intermediates&co folders ... So we need to use a different P4 workspace that match the same location but limited to folder we want to submit
+			// which will result in adding saved/intermediates&co folders ... So we need to use a different P4 workspace that match the same location but limited to folder we want to submit
 			p4publish credential: perforceCredentialInJenkins,
 				publish: submit(delete: false, description: "[ue4] ${env.JOB_NAME} ${env.BUILD_NUMBER} ${optionFullRebuild?'rebuild':''}".toString(), onlyOnSuccess: false, purge: '', reopen: false),
 				workspace: staticSpec(charset: perforceUnicodeMode, name: perforceWorkspaceNameForPublish, pinHost: false)
-			/*
-			// Start by reverting unchanged files
-			//p4.run('revert', '-a') // workspace should be setup to revert unchanged
-			
-			if(optionFullRebuild || engineOnly) // for engine only we also split in multiple CL
-			{
-				// Start by submitting all files except PDB in a single changelist
-				String[] subpaths = [
-					"${engineLocalPath}/....dll",
-					"${engineLocalPath}/....exe",
-					"${engineLocalPath}/....target",
-					"${engineLocalPath}/....modules",
-					"${engineLocalPath}/....version",
-					"${engineLocalPath}/....config",
-					"${projectLocalPath}/....dll",
-					"${projectLocalPath}/....exe",
-					"${projectLocalPath}/....target",
-					"${projectLocalPath}/....modules",
-					"${projectLocalPath}/....version",
-					"${projectLocalPath}/....config"
-					]
 
-				def value = CreateCLFromDefaultFilterered(p4, perforceWorkspaceName, "[ue4] ${env.JOB_NAME} ${env.BUILD_NUMBER} ${optionFullRebuild?'rebuild':''}".toString(), subpaths)
-				println ("Submit CL: " + value)
-				//p4.run('submit', '-c', value, '--parallel=threads=8,batch=4')
-				p4.run('submit', '-c', value)
-			}
-			else
-			{
-				//p4.run('submit', '-d', "[ue4] ${projectName}: ${env.JOB_NAME} ${env.BUILD_NUMBER}".toString(), '--parallel=threads=8,batch=4', '//...')
-				p4.run('submit', '-d', "[ue4] ${projectName}: ${env.JOB_NAME} ${env.BUILD_NUMBER} ${optionFullRebuild?'rebuild':''}".toString(), '//...')
-			}*/
+			// Updating the Perforce Label.
+			// Note that here we use the P4 workspace used to compile, so the Label will contains all folders that are maps to that workspace
+			// as the P4 plugin doesn't current allow to specify the mapping in the command.
+			p4tag credential: perforceCredentialInJenkins,
+				rawLabelDesc: perforceLabelDescription,
+				rawLabelName: perforceLabelName
 		}
-
-		/*
-		// unnecessary since we use p4publish
-		stage('Submit (PDB)')
-		{
-			if(optionFullRebuild || engineOnly) // for engine only we also split in multiple CL
-			{
-				//echo "Submit PDB (parallel)"
-				//p4.run('submit', '-d', "[ue4] ${env.JOB_NAME} ${env.BUILD_NUMBER} (PDB)".toString(), '--parallel=threads=8,batch=4', '//...')
-				// Failed with "Parallel file transfer must be enabled using net.parallel.max"
-				
-				
-				// For full rebuild, in order to avoid the "5 minutes command abort" issue, we split in multiple changelist
-				String[] subpaths = [
-					"${engineLocalPath}/Engine/Binaries/Win64/Android/...",
-					"${engineLocalPath}/Engine/Binaries/Win64/HTML5/...",
-					"${engineLocalPath}/Engine/Binaries/Win64/IOS/...",
-					"${engineLocalPath}/Engine/Binaries/Win64/Lumin/...",
-					"${engineLocalPath}/Engine/Binaries/Win64/ShaderCompileWorker*.pdb",
-					"${engineLocalPath}/Engine/Binaries/Win64/UnrealHeaderTool*.pdb",
-					"${engineLocalPath}/Engine/Binaries/...",
-					"${engineLocalPath}/Engine/Plugins/...",
-					"${engineLocalPath}/Engine/...",
-					"${projectLocalPath}/..."
-					]
-
-				subpaths.each {
-					// Adding some output in order to track which reconcile need to be split further in order to avoid abort error.
-					echo "Launching submit on ${it}"
-
-					// -d		Immediately submit the default changelist with the description supplied on the command line, and bypass the interactive form. This option is useful when scripting, but does not allow for jobs to be added, nor for the default changelist to be modified.
-					p4.run('submit', '-d', "[ue4] ${env.JOB_NAME} ${env.BUILD_NUMBER} ${it} ${optionFullRebuild?'rebuild':''}(PDB)".toString(), it)
-				}
-			}
-		}*/
 
 		def previousBuildStatus = GetPreviousBuildStatusExceptAborted()
 		def previousBuildSucceed = (previousBuildStatus == 'SUCCESS')
